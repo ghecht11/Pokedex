570e29c9b31dd7db7edaede72328cd30d85859d7
NachoDaCosta <nacho5981@gmail.com>
2022-10-05 20:30:06 -0300

    empezando a armar los datos

--- src/Componentes/PokemonList.jsx
+++ src/Componentes/PokemonList.jsx
@@ -1,19 +1,49 @@
 import React from "react";
+import Pokeapi from "../main";
+
 const PokemonList=()=>{
     return(
-        <div className="pokeList">
-            <div className="Pokemon-Container ghost">
-                <div className="pokemon-number"> 
-                    #001
+        <>
+        <Pokeapi/>
+            <div className="pokeList">
+                
+                <div className="Pokemon-Container grass">
+                    <div className="pokemon-number"> 
+                        #001
+                    </div>
+                    <div className="pokemon-image">
+                        <img src="./Imagenes/bulbasaur.png" alt=""/>
+                    </div>
+                    <div className="pokemon-name">
+                        <span>Bulbasaur</span>
+                    </div>
                 </div>
-                <div className="pokemon-image">
-                    <img src="./Imagenes/bulbasaur.png" alt=""/>
+
+                <div className="Pokemon-Container electric">
+                    <div className="pokemon-number"> 
+                        #025
+                    </div>
+                    <div className="pokemon-image">
+                        <img src="./Imagenes/pikachu.png" alt=""/>
+                    </div>
+                    <div className="pokemon-name">
+                        <span>Pikachu</span>
+                    </div>
                 </div>
-                <div className="pokemon-name">
-                    <span>Bulbasaur</span>
+
+                <div className="Pokemon-Container fire">
+                    <div className="pokemon-number"> 
+                        #025
+                    </div>
+                    <div className="pokemon-image">
+                        <img src="./Imagenes/charmander.png" alt=""/>
+                    </div>
+                    <div className="pokemon-name">
+                        <span>Charmander</span>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
