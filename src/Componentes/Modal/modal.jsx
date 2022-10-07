e708665065eb0bd1bf1ce9353320ed09ba5a1b42
NachoDaCosta <nacho5981@gmail.com>
2022-10-07 00:02:33 -0300

    info del json al modal ok

--- src/Componentes/Modal/modal.jsx
+++ src/Componentes/Modal/modal.jsx
@@ -1,48 +1,89 @@
 import React from "react";
+import Data from "./db.json"
+
+
 const Modal =()=>{
+        const Pokemons=Data;
+        console.log("hola");
     return(
-        <div className="Modal-grid">
+        
+        <>
+         {Pokemons.pokemones.map((pokemon,index)=>{ 
+            return (
+        <div className={`Modal-grid ${pokemon.background}`}>
             <div className="modal-name"> 
                 <div className="modal-1row">
                     <div>
-                        <img src="./Imagenes/arrow-left.svg" className="arrow-modal" alt=""/>
+                        <img src="./Imagenes/arrow-left.svg" className="white" alt=""/>
                     </div>
 
-                    <div> Charmander</div>
+                    <div>{pokemon.name}</div>
 
                     <div>
-                        <span>#004</span>
+                        #{pokemon.id}
                     </div>
                 </div>
 
-                {/* Comienza el segundo div del contenedor de nombre */}
-                <div className="modal-img">
-                    <div className="white">
-                        <img src="./Imagenes/arrow-left.svg" alt=""/>
+              
+                <div className="modal-2row">
+                    <div className="white arrow">
+                        <img src="./Imagenes/frame.svg" className="arrow-right " alt=""/>
                     </div>
 
-                    <div>
-                        <img src="/Imagenes/charmander.png" alt=""/>
+                    <div className="div-poke-img">
+                        <img src={pokemon.image} className="modal-poke-img" alt="" />
                     </div>
 
-                    <div className="white arrow-right">
-                        <img src="./Imagenes/arrow-left.svg" alt="" />
+                    <div className="white arrow">
+                        <img src="./Imagenes/frame.svg"  alt="" />
                     </div>
                 </div>
             </div>
             
             <div className="modal-bottom">
-                <div className="modal-type">
-                    <span>Fire</span>
+
+
+                <div className={`modal-type ${pokemon.background}`}>
+                    <span>{pokemon.type}</span>
                 </div>
+
                 
-                <div> about</div>
-                <div> peso y esas cosas</div>
-                <div> descripcion</div>
-                <div>base stats</div>
+                <div className={`about ${pokemon.type}`}> 
+                    About
+                </div>
+
+
+                <div className="modal-info"> 
+                    <div className="tworows line">
+                        <div className="twocols">
+                            <div><img src="/Imagenes/Weight.svg"/></div>
+                            <div className="bolder">{pokemon.weight}</div>    
+                        </div>
+                        <div>Weight</div>
+                    </div>
+
+                    <div className="tworows line">
+                        <div className="twocols">
+                            <div><img src="/Imagenes/height.svg"/></div>
+                            <div className="bolder">{pokemon.height}</div>
+                        </div>
+                        <div>Height</div>
+                    </div>
+
+                    <div className="tworows">
+                            <div>{pokemon.moves} </div>
+                            <div>Moves</div>
+                    </div>
+                </div>
+                <div className="modal-description"> {pokemon.description}</div>
+                <div className="about">base stats</div>
                 <div>stats</div>
             </div>
         </div>
+        )})}
+    </>
     )
 }
+    
+
 export default Modal;
